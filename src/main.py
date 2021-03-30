ea8839a3a013eac79a928249e95b26dcdb9a54f3
Brandon Fernández <andreyfdez18@gmail.com>
2021-03-30 16:47:47 +0000

    adding models

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User , Todo
+
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +39,42 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/todo', methods=['GET'])
+def handle_todo():
+    todo_all = Todo.query.all()
+   
+    my_todo= list(map(lambda x:x.serialize(), todo_all))
+    return jsonify(my_todo), 200
+
+@app.route('/todo', methods=['POST'])
+def add_todo():
+
+    # First we get the payload json
+    body = request.get_json()
+
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'label' not in body:
+        raise APIException('You need to specify the tasks', status_code=400)        
+
+    # at this point, all data has been validated, we can proceed to inster into the bd
+    new_Todo = Todo(label=body['label'], is_done=body['done'])
+    db.session.add(new_Todo)
+    db.session.commit()
+    return "ok", 200
+
+@app.route('/todo/<int:id>', methods=['DELETE'])
+def remove_todo(id):
+
+    item = Todo.query.get(id)
+    if item is None:
+        raise APIException('Task not found', status_code=404)
+
+    db.session.delete(item)
+    db.session.commit()
+
+    return jsonify("Task deleted successfully."), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,19 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(120), unique=True, nullable=False)
+    done = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Todo %r>' % self.label
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "label": self.label,
+            "done": self.done,
+            # do not serialize the password, its a security breach
+        }
